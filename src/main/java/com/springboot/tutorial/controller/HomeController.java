bcb1aa0933abf1144c028f5e1fda48d0dc0b6a74
Thiri <thiri@Thiris-MacBook-Air.local>
2020-04-13 17:21:09 +0300

    #3 オリジナルのログイン画面を実装

--- src/main/java/com/springboot/tutorial/controller/HomeController.java
+++ src/main/java/com/springboot/tutorial/controller/HomeController.java
@@ -1,14 +1,10 @@
 package com.springboot.tutorial.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-	@RequestMapping("/")
-    public String home(Model model){
-        return "index";
-    }
+public class HomeController{
 
+	
 }
