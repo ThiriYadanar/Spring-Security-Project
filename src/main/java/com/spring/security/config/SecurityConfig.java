487e6f34a0c86db2081ebf6c8944d1371c4799e5
Thiri <thiri@Thiris-MacBook-Air.local>
2020-04-27 17:44:55 +0300

    インデントをきれいにしました。 git commit -m インデントをきれいにしました。”

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.spring.security.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -21,89 +20,68 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 import com.spring.security.controller.CustomAccessDeniedHandler;
 import com.spring.security.controller.CustomAuthenticationSuccessHandler;
 
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private CustomAuthenticationSuccessHandler authenticationSuccessHandler;
-    
-    @Autowired
-    private CustomAccessDeniedHandler accessDeniedHandler;
-    
-    @Autowired
-    private AuthenticationManager authenticationManager;
-   
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	 http
-         .authorizeRequests()
-         .antMatchers( "/css/**").permitAll()
-         .antMatchers("/admin").hasRole("ADMIN")
-         .antMatchers("/expired").permitAll()
-         .anyRequest().authenticated()
-         .and()
-         .formLogin()
-         .loginPage("/login")
-         .successHandler(authenticationSuccessHandler)
-         .permitAll()
-         .and()
-         .logout()
-         .and()
-         .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
-         .and().csrf().disable();
-    	 http.formLogin()
- 		.loginPage("/login")
- 		.defaultSuccessUrl("/index", true)
- 		.failureHandler(customAuthenticationFailureHandler());
-         http.sessionManagement()
-     	.maximumSessions(1)               
-     	.maxSessionsPreventsLogin(true)    
-     	.expiredUrl("/login") 
-     	.sessionRegistry(sessionRegistry()) ;
-         http.sessionManagement().sessionFixation().migrateSession();
-         http.logout()
- 	    .invalidateHttpSession(true)
- 	    .deleteCookies("JSESSIONID")
- 		.permitAll();
-    }
-    
-    @Bean
-    SessionRegistry sessionRegistry() {			
-        return new SessionRegistryImpl();
-    }
-    
-    @Bean
-    public static ServletListenerRegistrationBean httpSessionEventPublisher() {	
-        return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
-    }
+	@Autowired
+	private CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+
+	@Autowired
+	private CustomAccessDeniedHandler accessDeniedHandler;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/**").permitAll().antMatchers("/admin").hasRole("ADMIN").anyRequest()
+				.authenticated().and().formLogin().loginPage("/login").successHandler(authenticationSuccessHandler)
+				.permitAll().and().logout().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler).and()
+				.csrf().disable();
+		http.formLogin().loginPage("/login").defaultSuccessUrl("/index", true)
+				.failureHandler(customAuthenticationFailureHandler());
+
+		http.sessionManagement().maximumSessions(1).maxSessionsPreventsLogin(true).expiredUrl("/login")
+				.sessionRegistry(sessionRegistry());
+		http.sessionManagement().sessionFixation().migrateSession();
+		http.logout().invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll();
+	}
 
 	@Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-      UserDetails user = User.withDefaultPasswordEncoder().username("user").password("pass").roles("USER").build();
-      UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("adpass").roles("ADMIN").build();
+	SessionRegistry sessionRegistry() {
+		return new SessionRegistryImpl();
+	}
 
-      return new InMemoryUserDetailsManager(user , admin);
-    }
-    
-    
-    @Bean
-    public CustomAccessDeniedHandler accessDeniedHandler(){
-        return new CustomAccessDeniedHandler();
-    }
-    
-    
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-    
-    @Bean
+	@Bean
+	public static ServletListenerRegistrationBean httpSessionEventPublisher() {
+		return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("pass").roles("USER").build();
+		UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("adpass").roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
+	@Bean
+	public CustomAccessDeniedHandler accessDeniedHandler() {
+		return new CustomAccessDeniedHandler();
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
 	public AuthenticationFailureHandler customAuthenticationFailureHandler() {
 		return new CustomAuthenticationFailureHandler();
 	}
-	
+
 }
